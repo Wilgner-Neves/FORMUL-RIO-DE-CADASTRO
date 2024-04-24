b4f30f90be2ceb2d54441a4d8c2efae4225d9659
Wilgner-Neves <153687406+Wilgner-Neves@users.noreply.github.com>
2024-04-23 22:13:56 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,40 @@
+const end = document.querySelector('#endereco')
+const bairro = document.querySelector('#bairro')
+const estado = document.querySelector('#estado')
+const cidade = document.querySelector('#cidade')
+const cep = document.querySelector('#cep')
+
+function preencheCampos(endereco) {
+    end.value = endereco.logradouro
+    bairro.value = endereco.bairro
+    estado.value = endereco.uf
+    cidade.value = endereco.localidade
+}
+
+function isValidCep (cep) {
+    return cep.length === 8 && /^[0-9]+$/.test(cep)
+}
+
+function buscarEndereco() {
+    const url = `https://viacep.com.br/ws/${cep.value}/json/`
+    if (isValidCep(cep.value)) {
+        fetch(url)
+        .then((response) => response.json())
+        .then((endereco) => {
+            if (endereco.erro) {
+            end.value = 'Endereço não encontrado!'
+            }
+            else {
+            preencheCampos(endereco)
+            }
+        })
+    }
+    else {
+        end.value = 'CEP inválido!'
+    }
+    
+
+}
+
+cep.addEventListener('blur', buscarEndereco)
+
